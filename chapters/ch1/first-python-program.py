73b75e54ab0226466079aa51293965da1dfe25ef
Samuel <olohye@gmail.com>
2022-08-27 23:35:42 -0400

    Challenge 1

---
+++ chapters/ch1/first-python-program.py
@@ -0,0 +1,14 @@
+print('sam fucks')
+
+my_name= 'sam oloh'
+
+print(my_name)
+
+#ask user for first name
+first_name = input('what is your first name')
+
+#ask second name
+last_name = input('what is your last name')
+
+
+print(f'Your full name is  {first_name} {last_name}')
