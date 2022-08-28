a2a029930a40b19963d2ceb1a196e440069a0d10
Samuel <olohye@gmail.com>
2022-08-27 23:54:28 -0400

    exercise 1

---
+++ chapters/ch1/exercise.py
@@ -0,0 +1,11 @@
+#ask user for season
+season = input('enter a season of the year')
+
+#ask user for number
+number = input('enter a number')
+
+#ask user for adjective
+adj = input('enter an adjective')
+
+print(f' On a {adj} {season} day, I drink at least {number} cups of juice')
+
